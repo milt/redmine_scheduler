0c2d87e97d1c9673ffca351e3413ad6eec8577c5
milt <milt@jhu.edu>
2011-07-25 18:53:27 -0400

    Revert d23590240bad3b8974c56fa714a938be2f8e48da^..HEAD

--- lib/user_patch.rb
+++ lib/user_patch.rb
@@ -1,7 +1,8 @@
 require_dependency 'user'
 
 # Patches Redmine's Users dynamically.  Adds a relationship 
-# User +has_and_belongs_to_many+ Skill
+# User +has_and_belongs_to_many+ to Skill
+
 module UserPatch
   def self.included(base) # :nodoc:
     base.extend(ClassMethods)
@@ -12,19 +13,25 @@ module UserPatch
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
       has_and_belongs_to_many :skills
-
+      
     end
 
   end
-
+  
   module ClassMethods
-
+    
   end
-
+  
   module InstanceMethods
-
-  end
+    # Wraps the association to get the Deliverable subject.  Needed for the 
+    # Query and filtering
+    #def deliverable_subject
+    #  unless self.deliverable.nil?
+    #    return self.deliverable.subject
+    #  end
+    #end
+  end    
 end
 
 # Add module to User
-User.send(:include, UserPatch)
+#User.send(:include, UserPatch)
