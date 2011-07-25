d5df2954e499208e651e85f86c214d0fd699439b
Milt <milt@jhu.edu>
2011-07-25 11:33:01 -0400

    Added redmine patches for HABTM relationships
    And got rid of the incorrect overwrites

---
+++ lib/issue_patch.rb
@@ -0,0 +1,36 @@
+require_dependency 'issue'
+
+# Patches Redmine's Issues dynamically.  Adds a relationship 
+# Issue +has_and_belongs_to_many+ to Deliverable
+module IssuePatch
+  def self.included(base) # :nodoc:
+    base.extend(ClassMethods)
+
+    base.send(:include, InstanceMethods)
+
+    # Same as typing in the class 
+    base.class_eval do
+      unloadable # Send unloadable so it will not be unloaded in development
+      has_and_belongs_to_many :skills
+      
+    end
+
+  end
+  
+  module ClassMethods
+    
+  end
+  
+  module InstanceMethods
+    # Wraps the association to get the Deliverable subject.  Needed for the 
+    # Query and filtering
+    #def deliverable_subject
+      #unless self.deliverable.nil?
+        #return self.deliverable.subject
+      #end
+    #end
+  end    
+end
+
+# Add module to Issue
+Issue.send(:include, IssuePatch)
