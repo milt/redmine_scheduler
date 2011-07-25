c8a376d3dc0a06c12a13b4b7e7ff6da1a463de18
Milt <milt@jhu.edu>
2011-07-25 09:58:34 -0400

    attempting to fix migration for Join table

--- db/migrate/3001_create_skills_users.rb
+++ db/migrate/3001_create_skills_users.rb
@@ -1,14 +1,12 @@
-class CreateSkillsUsers < ActiveRecord::Migration
+class CreateSkillUserJoinTable < ActiveRecord::Migration
   def self.up
     create_table :skills_users, :id => false do |t|
-      t.references :skill
-      t.references :user
+      t.integer :skill_id
+      t.integer :user_id
     end
-    add_index(:skills_users, [:skill_id, :user_id], :unique => true)
   end
 
   def self.down
     drop_table :skills_users
-    remove_index :skills_users
   end
 end
