563b332a684a1441882f53bb3c1d26d597f3f873
Milt <milt@jhu.edu>
2011-08-02 15:04:00 -0400

    Removed unnecessary fields from timeslot table. All values should be inhereited.

--- db/migrate/001_create_timeslots.rb
+++ db/migrate/001_create_timeslots.rb
@@ -2,10 +2,7 @@ class CreateTimeslots < ActiveRecord::Migration
   def self.up
     create_table :timeslots do |t|
       t.column :issue_id, :integer
-      t.column :booked, :boolean
-      t.column :slot_date, :date
       t.column :slot_time, :integer
-      t.column :coach_id, :integer
       
       t.timestamps
     end
