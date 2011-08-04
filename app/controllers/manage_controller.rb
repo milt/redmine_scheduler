605f15958f64769cf3466fbd179c2a637265c58d
milt <milt@jhu.edu>
2011-08-04 18:49:07 -0400

    more details for manage view

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -4,6 +4,9 @@ class ManageController < ApplicationController
 
   def index
     @allshifts = Tracker.find_by_name('Lab Coach Shift').issues.all
+    @alltimeslots = Timeslot.all
+    @allbookings = Booking.all
+    @unassigned_timeslots = @alltimeslots.reject {|timeslot| @allbookings.timeslot.exists?(timeslot)}
   end
 
   def schedule
