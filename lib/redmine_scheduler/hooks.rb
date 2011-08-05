29826bb2748c54abe22533606109ddb66c157207
Milt <milt@jhu.edu>
2011-08-05 10:40:01 -0400

    attempt to limit this behaviour to the lab coach tracker

--- lib/redmine_scheduler/hooks.rb
+++ lib/redmine_scheduler/hooks.rb
@@ -1,6 +1,9 @@
 class Hooks < Redmine::Hook::ViewListener
   def controller_issues_new_after_save(context={})
-    # Create a new timeslot for every issue
-    context[:issue].timeslots << Timeslot.create
+    # If the issue is on the Lab Coach tracker, make a timeslot
+    if context[:issue].tracker == 'Lab Coach Shift'
+      context[:issue].timeslots << Timeslot.create
+    else
+    end
   end
 end
