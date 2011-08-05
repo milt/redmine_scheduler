23bb4a63d7128bf75fb00f26ee646f85c36aeb75
Milt <milt@jhu.edu>
2011-08-05 10:42:55 -0400

    specify we are talking about name

--- lib/redmine_scheduler/hooks.rb
+++ lib/redmine_scheduler/hooks.rb
@@ -1,7 +1,7 @@
 class Hooks < Redmine::Hook::ViewListener
   def controller_issues_new_after_save(context={})
     # If the issue is on the Lab Coach tracker, make a timeslot
-    if context[:issue].tracker == 'Lab Coach Shift'
+    if context[:issue].tracker.name == 'Lab Coach Shift'
       context[:issue].timeslots << Timeslot.create
     else
     end
