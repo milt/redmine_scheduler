7376840a242b6bcbaf878216e457521592e2fd8d
milt <milt@jhu.edu>
2011-08-16 16:31:45 -0400

    Implemented search of avail slots in booking, cleaned up display in skills
    weee!

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -3,25 +3,28 @@ class BookingController < ApplicationController
 
 
   def index
-    unless session[:selected].nil?
-      @selected = session[:selected]
-      @skills = Skill.find_each(@selected)
+    #check for incoming search parameters, and instantiate an empty array if there are none.
+    if params[:skill_ids].nil?
+      @filter = []
+    else
+      @filter = params[:skill_ids]
     end
+    
     @timeslots = Timeslot.all
+    @skills = Skill.all
+    @selskills= @skills.select {|s| @filter.include?(s.id.to_s)}
     @unbooked = @timeslots.select {|t| t.booking.nil? && (t.slot_date > Date.today)}
-    @allskills = Skill.all
-  end
-
-  def checkskill
-    @skill = Skill.find(params[:skill_id])
-    session[:selected] << @skill[:id]
-    redirect_to :action => 'index'
-  end
-  
-  def uncheckskill
-    @skill = Skill.find(params[:skill_id])
-    session[:selected].delete(@skill[:id])
-    redirect_to :action => 'index'
+    @selslots = []
+    
+    
+    @selskills.each do |skill|
+      skill.timeslots.each do |slot|
+        unless @selslots.include?(slot)
+          @selslots << slot
+        end
+      end
+    end
+      
   end
   
   def book
