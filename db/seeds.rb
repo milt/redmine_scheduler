4e1ed5f9a6e0b68b8d30148744b185f97ba80e66
milt <milt@jhu.edu>
2012-01-29 00:48:41 -0500

    Update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 
 #load authentication method for PAM
 
-pam = AuthSource.new(:name => "PAM", :host => "localhost", :port => 1, :account => "user", :account_password => "pass", :base_dn => "app", :attr_login => "name", :attr_firstname => "firstname", :attr_lastname => "lastname", :attr_mail => "email", :onthefly_register => 1, :tls => 0)
+pam = AuthSource.new(:name => "PAM", :host => "localhost", :port => 1, :account => "user", :account_password => "pass", :base_dn => "app", :attr_login => "name", :attr_firstname => "firstname", :attr_lastname => "lastname", :attr_mail => "email", :onthefly_register => false, :tls => false)
 pam.type = "AuthSourcePam"
 pam.save
