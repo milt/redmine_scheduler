de19eb00b427d7a664f56a0647cafea16c886b98
Milton Reder <milt@jhu.edu>
2012-06-21 15:34:48 -0400

    removed migrate demo and accidentally created project

--- g/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'be99d804768b04a56cb8f0e864b077dc11a1ff66bd573b00c4c8f968142974088e81460b37da299c0df3bd2286105b0bd94f6ec3b3887860a0b0a7f9c997a11c';
