39cf0f6f4bec2a6ef42cd8dfd30db542c11bfaea
Milton Reder <milt@jhu.edu>
2012-07-24 14:15:19 -0400

    paid_time_wage needs to be a float

--- db/migrate/015_add_paid_time_wage_on_timesheets.rb
+++ db/migrate/015_add_paid_time_wage_on_timesheets.rb
@@ -1,6 +1,6 @@
 class AddPaidTimeWageOnTimesheets < ActiveRecord::Migration
   def self.up
-    add_column :timesheets, :paid_time_wage, :integer
+    add_column :timesheets, :paid_time_wage, :float
   end
 
   def self.down
