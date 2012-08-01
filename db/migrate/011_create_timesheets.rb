5a1dd462d01f7b30c026d4bd3125a3981c3ca71c
Milton Reder <milt@jhu.edu>
2012-08-01 15:20:24 -0400

    changed migrations for timesheets

--- db/migrate/011_create_timesheets.rb
+++ db/migrate/011_create_timesheets.rb
@@ -2,10 +2,12 @@ class CreateTimesheets < ActiveRecord::Migration
   def self.up
     create_table :timesheets do |t|
       t.column :user_id, :integer
-      t.column :pay_period, :datetime
+      t.column :weekof, :datetime
       t.column :print_date, :datetime
-      t.column :paid, :boolean
+      t.column :submitted, :datetime
+      t.column :approved, :datetime
       t.column :notes, :text
+      t.column :paid_time_wage, :float
       
       t.timestamps
     end
--- db/migrate/015_add_paid_time_wage_on_timesheets.rb
+++
@@ -1,9 +0,0 @@
-class AddPaidTimeWageOnTimesheets < ActiveRecord::Migration
-  def self.up
-    add_column :timesheets, :paid_time_wage, :float
-  end
-
-  def self.down
-    remove_column :timesheets, :paid_time_wage
-  end
-end
