4a66fac17b20fa8c08ec1c6aa5fd22317007762b
Milton Reder <milt@jhu.edu>
2012-08-08 10:49:51 -0400

    basic skeletal shift search

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -2,9 +2,25 @@ class TimeslotsController < ApplicationController
   unloadable
 
   def index
-  	@timeslots = Timeslot.after_now
+    @timeslots = Timeslot.after_now
   end
 
   def book
+  end
+
+  def find
+    if params[:from].present?
+      @from = Date.new(params[:from][:year].to_i,params[:from][:month].to_i,params[:from][:day].to_i)
+    else
+      @from = Date.today
+    end
+
+    if params[:to].present?
+      @to = Date.new(params[:to][:year].to_i,params[:to][:month].to_i,params[:to][:day].to_i)
+    else
+      @to = Date.today + 4.weeks
+    end
+
+    @shifts = Issue.lcshift.from_date(@from).until_date(@to)
   end
 end
