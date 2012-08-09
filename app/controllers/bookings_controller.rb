a3d9e71ac1cace029c872cdb31749d7e8e4566ef
Milton Reder <milt@jhu.edu>
2012-08-09 16:55:00 -0400

    rudimentary booking creation with multiple slots

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,63 @@
 class BookingsController < ApplicationController
   unloadable
-
+  before_filter :find_timeslots, :only => [:new, :create]
+  before_filter :find_booking, :only => [:show, :edit]
 
   def index
+    @bookings = Booking.all
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(params[:booking])
+    @booking.timeslots << @timeslots
+
+    respond_to do |format|
+      if @booking.save
+        flash[:notice] = 'Booking was successfully created.'
+        format.html { redirect_to :action => "index" }
+        format.xml  { render :xml => @booking, :status => :created,
+                    :location => @booking }
+      else                                               
+        flash[:warning] = 'Invalid Options... Try again!'
+        format.html { redirect_to :action => "new" }
+        format.xml  { render :xml => @booking.errors,
+                    :status => :unprocessable_entity }
+      end
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def find_timeslots
+    if params[:slot_ids].present?
+      @timeslots = params[:slot_ids].map {|id| Timeslot.find(id)}
+    else
+      flash[:warning] = 'Cannot make a new Booking without some timeslots. Pick some here and click "Book..."'
+      redirect_to :controller => 'timeslots', :action => 'find'
+    end
+
+    if @timeslots.detect {|slot| slot.booked?}
+      flash[:warning] = 'One or more timeslots is already booked. Please select available slots here and click "Book..."'
+      redirect_to :controller => 'timeslots', :action => 'find'
+    end
+  end
+
+  def find_booking
+    if params[:id].present?
+      @booking = Booking.find(params[:id])
+    else
+      flash[:warning] = 'No ID specified.'
+      redirect_to :action => 'index'
+    end
   end
 end
