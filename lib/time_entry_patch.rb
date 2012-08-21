e9f99250262f20561e96e75b39e1b3306804fa34
Milton Reder <milt@jhu.edu>
2012-08-21 10:44:41 -0400

    duh. fixed locking of timesheets

--- lib/time_entry_patch.rb
+++ lib/time_entry_patch.rb
@@ -17,13 +17,13 @@ module TimeEntryPatch
       named_scope :on_tyear, lambda {|d| { :conditions => ["tyear = ?", d] } }
       named_scope :sort_by_date, :order => "spent_on ASC"
       validate_on_create :cannot_create_if_timesheet_exists
-      # validate_on_update :locked_when_attached_to_timesheet, :unless => "timesheet_id.present?"
+      validate_on_update :locked_when_attached_to_timesheet
 
       
-      # def locked_when_attached_to_timesheet
-      #   errors.add_to_base("Cannot edit because this time entry is attached to timesheet #{self.timesheet_id}.") if
-      #     !timesheet_id.blank?
-      # end
+      def locked_when_attached_to_timesheet
+        errors.add_to_base("Cannot edit because this time entry is attached to timesheet #{self.timesheet_id}.") if
+          Timesheet.for_user(self.user).weekof_on(self.spent_on.monday).is_submitted.present?
+      end
 
       def cannot_create_if_timesheet_exists
         errors.add_to_base("Cannot create time entry because there is already a timesheet submitted for this date.") if
