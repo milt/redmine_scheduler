3af90a86d7d1c845a1d7ad028723f11e19a8f255
Milton Reder <milt@jhu.edu>
2012-08-31 12:20:31 -0400

    more work on student_levels, fixed assignment of approve_time_wage

--- app/controllers/prostaff_controller.rb
+++ app/controllers/prostaff_controller.rb
@@ -8,5 +8,6 @@ class ProstaffController < ApplicationController
   def student_levels
     @students = Group.stustaff.first.users.active
     @training_project = Project.find(:first, :conditions => "name = 'Training Project'")
+    @goals_by_student = Issue.goals.open.group_by(&:assigned_to)
   end
 end
