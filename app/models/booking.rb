f50226d991792ec7af3276e8d38eff735c0d0ba1
Guannan Ren <gren3@jhu.edu>
2013-01-20 17:30:48 -0500

    Added a minimum character limit to booking description, took out the maximum character limit.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,7 +7,7 @@ class Booking < ActiveRecord::Base
   validates_presence_of :name, :phone, :email, :project_desc #all form fields must exist be non-nil 
   validates_length_of :name, :email, :maximum => 127 #name can't be over 127 chars
   validates_length_of :phone, :maximum => 16 # phone max length of 16 chars
-  validates_length_of :project_desc, :maximum => 1024 #limit the description field
+  validates_length_of :project_desc, :minimum => 1 #took away max limit of 1024 char in the description field
   default_scope :order => 'apt_time ASC' #default sort order of Bookings
   named_scope :future, lambda { { :conditions => ["apt_time >= ?", Date.today] } }
   #named_scope :booked, lambda { { :conditions => "timeslot_id IS NOT NULL" } }
