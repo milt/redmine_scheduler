6f5aed899940d48f3b07626f2ccdd0c9b6ea662b
Guannan Ren <gren3@jhu.edu>
2013-01-23 15:55:39 -0500

    fixed the booking description length to have a max count again.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,12 +7,11 @@ class Booking < ActiveRecord::Base
   validates_presence_of :name, :phone, :email, :project_desc #all form fields must exist be non-nil 
   validates_length_of :name, :email, :maximum => 127 #name can't be over 127 chars
   validates_length_of :phone, :maximum => 16 # phone max length of 16 chars
-  validates_length_of :project_desc, :minimum => 1 #took away max limit of 1024 char in the description field
+  validates_length_of :project_desc, :maximum => 4096, :allow_blank => true #allow blank is not working
   default_scope :order => 'apt_time ASC' #default sort order of Bookings
   named_scope :future, lambda { { :conditions => ["apt_time >= ?", Date.today] } }
-  #named_scope :booked, lambda { { :conditions => "timeslot_id IS NOT NULL" } }
-  named_scope :cancelled, lambda { { :conditions => { :cancelled => true } } }
-  named_scope :orphaned, lambda { { :conditions => { :cancelled => false } } }
+  named_scope :cancelled, lambda {{:conditions => {:cancelled => true }}}
+  named_scope :orphaned, lambda {{:conditions => {:cancelled => false }}}
   named_scope :from_date, lambda {|d| {:conditions => ["apt_time >= ?", d]}}    #similar to from/until date in issue_patch.rb
   named_scope :until_date, lambda {|d| {:conditions => ["apt_time <= ?", d+1]}}  #weird occurrance when using d, misses bookings on the until date
   validate_on_create :cannot_create_across_issues, :cannot_create_without_timeslots
@@ -33,13 +32,6 @@ class Booking < ActiveRecord::Base
     errors.add_to_base("Bookings must have at least one timeslot") if
           self.timeslots.empty? && self.cancelled.nil?
   end
-
-  #not really being useful
-  # def self.from_time(date)
-  #   bookings = []
-  #   Booking.from_date(date).map{|i| bookings << i}
-  #   bookings  #returns bookings
-  # end
 
   def set_apt_time
     self.apt_time = self.timeslots.sort_by(&:slot_time).first.start_time
@@ -64,13 +56,5 @@ class Booking < ActiveRecord::Base
     self.cancelled = false
     self.save
   end
-
-  # def coach
-  #   if self.timeslots.present?
-  #     self.timeslots.first.coach
-  #   else
-  #     return "dunno"
-  #   end
-  # end
 
 end
