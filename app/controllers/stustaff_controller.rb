5d08079d9039d735dc7476ee1906b2975e272fc1
Guannan <gren3@jhu.edu>
2013-03-18 19:31:31 -0700

    task pool table added to stustaff index page. awaiting for further instruction. there might be the need for a take task command.

--- app/controllers/stustaff_controller.rb
+++ app/controllers/stustaff_controller.rb
@@ -10,5 +10,6 @@ class StustaffController < ApplicationController
     @mytasks = Issue.open.tasks.foruser(User.current)
     @mygoals = Issue.open.goals.foruser(User.current)
     @mywatches = Issue.visible.watched_by(User.current)  #this line should returns an array of watched issues for current user
+    @taskpool = Issue.find(:all,:conditions=>{:assigned_to_id=>nil})
   end
 end
