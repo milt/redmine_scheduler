266e9d55de05172b355b03ebef1bea44657e743b
Guannan <gren3@jhu.edu>
2013-04-04 21:58:32 -0700

    Edited the stustaff taskpool table to only allow issues from the task tracker. Also started the my_page development of unassigned repair tasks.

--- app/controllers/stustaff_controller.rb
+++ app/controllers/stustaff_controller.rb
@@ -10,6 +10,6 @@ class StustaffController < ApplicationController
     @mytasks = Issue.open.tasks.foruser(User.current)
     @mygoals = Issue.open.goals.foruser(User.current)
     @mywatches = Issue.visible.watched_by(User.current)  #this line should returns an array of watched issues for current user
-    @taskpool = Issue.find(:all,:conditions=>{:assigned_to_id=>nil})
+    @taskpool = Issue.find(:all,:conditions=>{:assigned_to_id=>nil,:tracker_id=>Tracker.task_track.first.id})
   end
 end
