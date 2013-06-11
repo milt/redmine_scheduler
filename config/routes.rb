3e4f3f155c9decb004ca69c1e91fd2774dc1c05f
Milton Reder <milt@jhu.edu>
2013-06-11 17:03:36 -0400

    numerous upgradey bits

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,32 @@
 RedmineApp::Application.routes.draw do
-  resources :repairs
+  resources :repairs, :skillcats, :skills, :wages
+
+  resources :bookings do
+    member do
+      put 'cancel'
+    end
+  end
+
+  resources :levels do
+    member do
+      get 'my_levels'
+      post 'bulk_create'
+    end
+  end
+
+  resources :timesheets do
+    member do
+      post 'print'
+      put 'submit', 'approve', 'reject'
+    end
+  end
+
+  resources :timeslots do
+    member do
+      get 'find'
+      get 'book'
+    end
+  end
 
   get 'stustaff', to: 'stustaff#index'
 
