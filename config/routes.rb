f9079a7770f1e28f98e3094e0d001c39feaeb300
Milton Reder <milt@jhu.edu>
2013-06-11 15:39:02 -0400

    home page loads

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 RedmineApp::Application.routes.draw do
   resources :repairs
+
+  get 'stustaff', to: 'stustaff#index'
+
+  get 'prostaff', to: 'prostaff#index'
+  get 'student_levels', to: 'prostaff#student_levels'
+  get 'admstaff', to: 'admstaff#index'
 end
 
