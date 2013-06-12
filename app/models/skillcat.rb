24e5d570212d891eda4196d440a3eb4def530b1f
Milt Reder <milt@jhu.edu>
2013-06-12 10:07:50 -0400

    further improvements to skillcat

--- app/models/skillcat.rb
+++ app/models/skillcat.rb
@@ -5,8 +5,10 @@ class Skillcat < ActiveRecord::Base #Skill categories
   validates :name, uniqueness: true #skill category names are unique
   default_scope :order => 'name ASC' #default sort is category name ascending
   attr_accessible :name, :descr
-  scope :auth_cat, lambda { { :conditions => ["name = ?", "Authorization"] } }
-  scope :skill_cat, lambda { { :conditions => ["name != ?", "Authorization"] } }
+  #scope :auth_cat, lambda { { :conditions => ["name = ?", "Authorization"] } }
+  scope :auth_cat, where( name: "Authorization" )
+  #scope :skill_cat, lambda { { :conditions => ["name != ?", "Authorization"] } }
+  scope :skill_cat, where( name: "General" )
 
   def auth?
   	if name == "Authorization"
