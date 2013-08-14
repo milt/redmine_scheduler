79708e451203807e32eab375d77f695b633a4492
Milton Reder <milt@jhu.edu>
2013-08-14 14:02:14 -0400

    REALLY fixed rebuild script

--- script/rebuild.bash
+++ script/rebuild.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-rm db/development.sqlite3
+rm db/redmine_development.sqlite3
 rake db:migrate
 rake redmine:load_default_data
 rake redmine:plugins:migrate
 rake redmine_scheduler:load_default_data
+rails r plugins/redmine_scheduler/script/add_people_and_groups.rb
 rails r plugins/redmine_scheduler/script/add_shifts.rb
 
 exit 0
