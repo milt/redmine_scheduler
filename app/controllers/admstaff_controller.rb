53fbdef570fab378222b77a69f5b12079d669822
Milt Reder <milt@jhu.edu>
2013-08-17 16:34:06 -0400

    nifty admin notifications

--- app/controllers/admstaff_controller.rb
+++ app/controllers/admstaff_controller.rb
@@ -3,5 +3,25 @@ class AdmstaffController < ApplicationController
 
   def index
     authorize! :index, :admin
+
+    notification_check
+  end
+
+  private
+
+  def notification_check
+    @notifications = {}
+
+    if Booking.orphaned.present?
+      @notifications["Orphaned bookings detected. Please resolve them."] = bookings_path
+    end
+
+    if Fine.unpaid.present?
+      @notifications["There are unpaid fines."] = fines_path
+    end
+
+    if Timesheet.is_submitted.is_not_approved.present?
+      @notifications["There are submitted timesheets awaiting your approval!"] = timesheets_path
+    end
   end
 end
