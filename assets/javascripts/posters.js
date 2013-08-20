135461bd74a632992967a7ac837c6df6ea8cda65
Milt Reder <milt@jhu.edu>
2013-08-20 13:45:29 -0400

    some js for posters

--- assets/javascripts/posters.js
+++ assets/javascripts/posters.js
@@ -1,4 +1,47 @@
 $(document).ready(function(){
+
+  //handle document size calculation
+  var updateDocAspect = function() {
+    var docWidth = parseFloat($("#poster_doc_width").val());
+    var docHeight = parseFloat($("#poster_doc_height").val());
+    var docRatio = docWidth / docHeight;
+    //round to 1 decimal place
+    docRatio = Math.round(docRatio*10)/10;
+    if (docWidth && docHeight){
+     $("#doc_aspect").val(docRatio);
+    }
+  };
+
+  $("#poster_doc_width").change(function(){
+    updateDocAspect();
+  });
+
+  $("#poster_doc_height").change(function(){
+    updateDocAspect();
+  });
+
+  //handle print size calculation
+  var updatePrintAspect = function() {
+    var printWidth = parseFloat($("#poster_print_width").val());
+    var printHeight = parseFloat($("#poster_print_height").val());
+    var printRatio = printWidth / printHeight;
+    //round to 1 decimal place
+    printRatio = Math.round(printRatio*10)/10;
+    if (printWidth && printHeight){
+     $("#print_aspect").val(printRatio);
+    }
+  };
+
+  $("#poster_print_width").change(function(){
+    updatePrintAspect();
+  });
+
+  $("#poster_print_height").change(function(){
+    updatePrintAspect();
+  });
+
+  
+
   $("#poster_affiliation").change(function(){
     // alert( "Handler for .change() called." );
   });
