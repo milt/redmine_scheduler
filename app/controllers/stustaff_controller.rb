de85d16eefe544e6d02623decac773fc9c0dccbb
Milt Reder <milt@jhu.edu>
2013-09-08 18:30:24 -0400

    fixed task pool with closed stuff

--- app/controllers/stustaff_controller.rb
+++ app/controllers/stustaff_controller.rb
@@ -10,7 +10,7 @@ class StustaffController < ApplicationController
     @mytasks = Issue.open.tasks.foruser(User.current)
     @mygoals = Issue.open.goals.foruser(User.current)
     @mywatches = Issue.visible.watched_by(User.current)  #this line should returns an array of watched issues for current user
-    @taskpool = Issue.find(:all,:conditions=>{:assigned_to_id=>nil,:tracker_id=>Tracker.task_track.first.id})
+    @taskpool = Issue.open.find(:all,:conditions=>{:assigned_to_id=>nil,:tracker_id=>Tracker.task_track.first.id})
     @reminders = Reminder.all
   end
 end
